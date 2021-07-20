4a05250ff1c6b0faf2bb43ce8cfe259e79b7d0e5
aviral373 <aviral373@gmail.com>
2021-07-20 23:06:04 +0530

    made Tetris.py

---
+++ Tetris.py
@@ -0,0 +1,225 @@
+import random
+import pygame
+
+"""
+10 x 20 grid
+play_height = 2 * play_width
+tetriminos:
+    0 - S - green
+    1 - Z - red
+    2 - I - cyan
+    3 - O - yellow
+    4 - J - blue
+    5 - L - orange
+    6 - T - purple
+"""
+
+pygame.font.init()
+
+# global variables
+
+col = 10  # 10 columns
+row = 20  # 20 rows
+s_width = 800  # window width
+s_height = 750  # window height
+play_width = 300  # play window width; 300/10 = 30 width per block
+play_height = 600  # play window height; 600/20 = 20 height per block
+block_size = 30  # size of block
+
+top_left_x = (s_width - play_width) // 2
+top_left_y = s_height - play_height - 50
+
+filepath = '/Users/rajat/PycharmProjects/Tetris/highscore.txt'
+fontpath = '/Users/rajat/PycharmProjects/Tetris/arcade.ttf'
+fontpath_mario = '/Users/rajat/PycharmProjects/Tetris/mario.ttf'
+
+# shapes formats
+
+S = [['.....',
+      '.....',
+      '..00.',
+      '.00..',
+      '.....'],
+     ['.....',
+      '..0..',
+      '..00.',
+      '...0.',
+      '.....']]
+
+Z = [['.....',
+      '.....',
+      '.00..',
+      '..00.',
+      '.....'],
+     ['.....',
+      '..0..',
+      '.00..',
+      '.0...',
+      '.....']]
+
+I = [['.....',
+      '..0..',
+      '..0..',
+      '..0..',
+      '..0..'],
+     ['.....',
+      '0000.',
+      '.....',
+      '.....',
+      '.....']]
+
+O = [['.....',
+      '.....',
+      '.00..',
+      '.00..',
+      '.....']]
+
+J = [['.....',
+      '.0...',
+      '.000.',
+      '.....',
+      '.....'],
+     ['.....',
+      '..00.',
+      '..0..',
+      '..0..',
+      '.....'],
+     ['.....',
+      '.....',
+      '.000.',
+      '...0.',
+      '.....'],
+     ['.....',
+      '..0..',
+      '..0..',
+      '.00..',
+      '.....']]
+
+L = [['.....',
+      '...0.',
+      '.000.',
+      '.....',
+      '.....'],
+     ['.....',
+      '..0..',
+      '..0..',
+      '..00.',
+      '.....'],
+     ['.....',
+      '.....',
+      '.000.',
+      '.0...',
+      '.....'],
+     ['.....',
+      '.00..',
+      '..0..',
+      '..0..',
+      '.....']]
+
+T = [['.....',
+      '..0..',
+      '.000.',
+      '.....',
+      '.....'],
+     ['.....',
+      '..0..',
+      '..00.',
+      '..0..',
+      '.....'],
+     ['.....',
+      '.....',
+      '.000.',
+      '..0..',
+      '.....'],
+     ['.....',
+      '..0..',
+      '.00..',
+      '..0..',
+      '.....']]
+
+# index represents the shape
+shapes = [S, Z, I, O, J, L, T]
+shape_colors = [(0, 255, 0), (255, 0, 0), (0, 255, 255), (255, 255, 0), (255, 165, 0), (0, 0, 255), (128, 0, 128)]
+
+
+# class to represent each of the pieces
+
+
+class Piece(object):
+    def __init__(self, x, y, shape):
+        self.x = x
+        self.y = y
+        self.shape = shape
+        self.color = shape_colors[shapes.index(shape)]  # choose color from the shape_color list
+        self.rotation = 0  # chooses the rotation according to index
+
+
+# initialise the grid
+def create_grid(locked_pos={}):
+    grid = [[(0, 0, 0) for x in range(col)] for y in range(row)]  # grid represented rgb tuples
+
+    # locked_positions dictionary
+    # (x,y):(r,g,b)
+    for y in range(row):
+        for x in range(col):
+            if (x, y) in locked_pos:
+                color = locked_pos[
+                    (x, y)]  # get the value color (r,g,b) from the locked_positions dictionary using key (x,y)
+                grid[y][x] = color  # set grid position to color
+
+    return grid
+
+
+def convert_shape_format(piece):
+    positions = []
+    shape_format = piece.shape[piece.rotation % len(piece.shape)]  # get the desired rotated shape from piece
+
+    '''
+    e.g.
+       ['.....',
+        '.....',
+        '..00.',
+        '.00..',
+        '.....']
+    '''
+    for i, line in enumerate(shape_format):  # i gives index; line gives string
+        row = list(line)  # makes a list of char from string
+        for j, column in enumerate(row):  # j gives index of char; column gives char
+            if column == '0':
+                positions.append((piece.x + j, piece.y + i))
+
+    for i, pos in enumerate(positions):
+        positions[i] = (pos[0] - 2, pos[1] - 4)  # offset according to the input given with dot and zero
+
+    return positions
+
+
+# checks if current position of piece in grid is valid
+def valid_space(piece, grid):
+    # makes a 2D list of all the possible (x,y)
+    accepted_pos = [[(x, y) for x in range(col) if grid[y][x] == (0, 0, 0)] for y in range(row)]
+    # removes sub lists and puts (x,y) in one list; easier to search
+    accepted_pos = [x for item in accepted_pos for x in item]
+
+    formatted_shape = convert_shape_format(piece)
+
+    for pos in formatted_shape:
+        if pos not in accepted_pos:
+            if pos[1] >= 0:
+                return False
+    return True
+
+
+# check if piece is out of board
+def check_lost(positions):
+    for pos in positions:
+        x, y = pos
+        if y < 1:
+            return True
+    return False
+
+
+# chooses a shape randomly from shapes list
+def get_shape():
+    return Piece(5, 0, random.choice(shapes))
+
